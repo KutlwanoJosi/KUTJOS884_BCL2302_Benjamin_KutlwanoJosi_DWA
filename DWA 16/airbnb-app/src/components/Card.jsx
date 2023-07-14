52ea4255c77e5a784293bb7206e3638823c3d2c0
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-07-14 16:01:03 +0200

    Fixed Star Image

--- DWA 16/airbnb-app/src/components/Card.jsx
+++ DWA 16/airbnb-app/src/components/Card.jsx
@@ -19,7 +19,7 @@ function Card(props) {
                 className="card--image" 
             />
             <div className="card--stats">
-                <img src="../images/star.png" className="card--star" />
+                <img src="./src/images/star.png" className="card--star" />
                 <span>{props.stats.rating}</span>
                 <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
